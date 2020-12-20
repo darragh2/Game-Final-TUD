fc44aaeb4cf21b8e8622dced6890c50fe3d85f76
darragh2 <72267924+darragh2@users.noreply.github.com>
2020-12-20 22:53:17 +0000

    Unity file

---
+++ Obstacle mania/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour{
+
+    public string Url = "https://github.com/darragh2/Game-Final-TUD";
+
+    public void GoToLevel1()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void GoToLevel2()
+    {
+        SceneManager.LoadScene("Level2");
+    }
+    public void GoToLevel3()
+    {
+        SceneManager.LoadScene("Level3");
+    }
+    public void GoToUrl()
+    {
+        Application.OpenURL(Url);
+    }
+}
---
+++ Obstacle mania/Assets/Scripts/stopwatch.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class stopwatch : MonoBehaviour { 
+
+    public float timeStart;
+    public Text textBox;
+
+    void Start()
+    {
+        textBox.text = timeStart.ToString("F2");
+    }
+    void Update()
+    {
+        timeStart += Time.deltaTime;
+        textBox.text = timeStart.ToString("F2");
+    }
+}
